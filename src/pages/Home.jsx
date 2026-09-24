9c1081967cb33cd2e3a8bc27cd2a08c5ee9aff41
Will Saffran <willh.saffran@gmail.com>
2026-09-24 19:29:11 -0400

    Full unlocked link removal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { days } from '../days'
 import { useProgress } from '../progress'
 
 const letteringSrc = `${import.meta.env.BASE_URL}chiaki-lettering-white.png`
@@ -37,7 +36,7 @@ function HomeLetter() {
 }
 
 export default function Home() {
-  const { allUnlocked, nextDay, canAttempt, selectDay } = useProgress()
+  const { nextDay, canAttempt, selectDay } = useProgress()
   const nextIsOpen = Boolean(nextDay) && canAttempt(nextDay.id)
   const copy = nextIsOpen
     ? `Continue with ${nextDay.label}`
@@ -64,18 +63,6 @@ export default function Home() {
             <p className="lede">{copy}</p>
           )}
         </div>
-        {allUnlocked ? (
-          <ul className="revisit-list">
-            {days.map((day) => (
-              <li key={day.id}>
-                <button type="button" onClick={() => selectDay(day.id)}>
-                  <span>{day.label}</span>
-                  <span className="revisit-title">{day.title}</span>
-                </button>
-              </li>
-            ))}
-          </ul>
-        ) : null}
       </main>
     </>
   )
